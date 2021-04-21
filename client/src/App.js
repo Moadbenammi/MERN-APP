71a50692620bc8555fc8296d5ed6a83aff1f528d
Moad BENAMMI <moadbenammi@gmail.com>
2021-04-21 22:38:33 +0000

    TP 3

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppNavBar from './components/AppNavBar';
-import ShoppingList from './components/ShoppingList';
+import AppNavBar from "./components/AppNavBar";
+import ShoppingList from "./components/ShoppingList";
+import store from "./store";
+import { Provider } from "react-redux";
+import { Container } from "reactstrap";
+import ItemModal from "./components/ItemModal";
+
 
 function App() {
   return (
-    <div className="App">
-      <AppNavBar/>
-      <ShoppingList />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavBar />
+        <Container>
+          <ItemModal />
+          <ShoppingList />
+        </Container>
+      </div>
+    </Provider>
   );
-}
+};
 
 export default App;
--- client/src/components/AppNavBar.js
+++ client/src/components/AppNavBar.js
@@ -9,7 +9,7 @@ import {
   NavLink,
 } from "reactstrap";
 
-const AppNavBar = (props) => {
+function AppNavBar (props) {
   const [isOpen, setIsOpen] = useState(false);
 
   const toggle = () => setIsOpen(!isOpen);
